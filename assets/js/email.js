481c6072eabe574fa8b780289e4d0b48aa48532f
Scott Mangiapane <scottmangiapane@gmail.com>
2016-08-19 08:00:09 -0700

    Email obfuscation
    Added some simple javascript to hide my email from bots.

---
+++ assets/js/email.js
@@ -0,0 +1,6 @@
+tag = 'mailto';
+colon = '&#x0003A;';
+user = 'contact';
+at = '&#x00040;';
+site = 'scottmangiapane.com';
+document.write("<a href='" + tag + colon + user + at + site + "'>" + "let me know." + "</a>")
