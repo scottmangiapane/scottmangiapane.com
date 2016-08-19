4e59e85ebb98190ce2fce673014ba9abdda13dff
Scott Mangiapane <scottmangiapane@gmail.com>
2016-08-19 18:17:01 -0500

    Adjusted Javascript warning, removed obsolete attribute
    Made the Javascript warning blend in more seamlessly with it’s
    surroundings; removed references to the obsolete ‘align’ attribute in
    the table.

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,4 +3,4 @@ colon = '&#x0003A;';
 user = 'contact';
 at = '&#x00040;';
 site = 'scottmangiapane.com';
-document.write("<a href='" + tag + colon + user + at + site + "'>" + "let me know." + "</a>")
+document.write("<p class='text-inline'><a href='" + tag + colon + user + at + site + "'>" + "let me know." + "</a></p>")
