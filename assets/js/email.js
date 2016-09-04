8907b3e230d3a39a576a5babca72f539b1d6cd17
Scott Mangiapane <scottmangiapane@gmail.com>
2016-09-04 14:53:00 -0400

    Icon hover; footer'; minor edits
    Added a slide animation to the down arrow icon when the mouse hovers
    over; removed 'made with <3; cleaned up random parts of the project.

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,4 +3,4 @@ colon = '&#x0003A;';
 user = 'contact';
 at = '&#x00040;';
 site = 'scottmangiapane.com';
-document.write("<a class='button' href='" + tag + colon + user + at + site + "'>" + "E-MAIL" + "</a>")
+document.write("<a class='button' href='" + tag + colon + user + at + site + "'>" + "E-MAIL" + "</a>");
