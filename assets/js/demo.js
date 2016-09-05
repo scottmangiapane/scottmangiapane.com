4cecd78473652bb8b97e561b785f1630f1a1dc85
Scott Mangiapane <scottmangiapane@gmail.com>
2016-09-05 11:43:39 -0400

    Change demo image
    Added javascript to change the demo-overlay image when the user mouses
    over the cards.

---
+++ assets/js/demo.js
@@ -0,0 +1,27 @@
+$(document).ready(function () {
+    var app_ca = $('#calculator-v2');
+    var app_cc = $('#card-cliques');
+    var app_qc = $('#quackcryption');
+    var demo = $('#demo-overlay');
+
+    app_ca.hover(function () {
+        var src = demo.attr("src")
+            .replace("card-cliques.svg", "calculator-v2.svg")
+            .replace("quackcryption.svg", "calculator-v2.svg");
+        demo.attr("src", src);
+    });
+
+    app_cc.hover(function () {
+        var src = demo.attr("src")
+            .replace("calculator-v2.svg", "card-cliques.svg")
+            .replace("quackcryption.svg", "card-cliques.svg");
+        demo.attr("src", src);
+    });
+
+    app_qc.hover(function () {
+        var src = demo.attr("src")
+            .replace("calculator-v2.svg", "quackcryption.svg")
+            .replace("card-cliques.svg", "quackcryption.svg");
+        demo.attr("src", src);
+    });
+});
