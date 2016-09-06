dbd7708f66a7d9debda5793e9fd4cecdd6bd35a9
Scott Mangiapane <scottmangiapane@gmail.com>
2016-09-05 20:55:45 -0400

    Cleaned animation
    Removed useless animation at page load.

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -4,9 +4,9 @@ $(document).ready(function () {
     var demo_cc = $('#demo-card-cliques');
     var demo_qc = $('#demo-quackcryption');
 
-    demo_js.css({opacity: 1}).animate({opacity: 0});
-    demo_cc.css({opacity: 1}).animate({opacity: 0});
-    demo_qc.css({opacity: 1}).animate({opacity: 0});
+    demo_js.css({opacity: 0});
+    demo_cc.css({opacity: 0});
+    demo_qc.css({opacity: 0});
 
     $('#card-calculator-v2').hover(function () {
         demo_ca.animate({
