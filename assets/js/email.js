8dba523abd95bfa1245771688ce2491b1b8f3ad4
Scott Mangiapane <scottmangiapane@gmail.com>
2016-09-08 21:19:06 -0400

    Changed email; image titles
    Changed email address from contact@… to email@…; removed .svg image
    titles.

--- assets/js/email.js
+++ assets/js/email.js
@@ -1,6 +1,6 @@
 tag = 'mailto';
 colon = '&#x0003a;';
-user = 'contact';
+user = 'email';
 at = '&#x00040;';
 site = 'scottmangiapane.com';
 document.write("<a class='button' href='" + tag + colon + user + at + site + "'>" + "E-MAIL" + "</a>");
