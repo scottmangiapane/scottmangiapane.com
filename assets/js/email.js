77fa05d6a5d9d3a670c354c81b40a932a99dc3b4
Scott Mangiapane <scottmangiapane@gmail.com>
2016-09-13 22:47:14 -0400

    Popup on all buttons
    Added popups to all the buttons in the about me section; changed popup
    formatting in the style.css file.

--- assets/js/email.js
+++ assets/js/email.js
@@ -3,4 +3,7 @@ colon = '&#x0003a;';
 user = 'email';
 at = '&#x00040;';
 site = 'scottmangiapane.com';
-document.write("<a class='button' href='" + tag + colon + user + at + site + "'>" + "E-MAIL" + "</a>");
+document.write("<span class='popup-wrapper'><a class='button' href='"
+    + tag + colon + user + at + site
+    + "'>" + "E-MAIL" + "</a><span class='popup'><p class='remove-margins'>"
+    + user + at + site + "</p></span></span>");
