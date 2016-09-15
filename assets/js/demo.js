2075b7f780bcdcc00c149fb297107448ea879a0f
Scott Mangiapane <scottmangiapane@gmail.com>
2016-09-15 19:12:08 -0400

    Animation duration
    Shortened duration of demo fading different screenshots together.

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -9,44 +9,44 @@ $(document).ready(function () {
     demo_qc.css({opacity: 0});
     demo_js.animate({
         opacity: 0
-    }, {duration: 250, queue: false});
+    }, {duration: 220, queue: false});
 
     $('#card-calculator-v2').hover(function () {
         demo_ca.animate({
             opacity: 1
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_cc.animate({
             opacity: 0
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_qc.animate({
             opacity: 0
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_link.attr('href', 'https://github.com/scottmangiapane/android-calculator-v2');
     });
 
     $('#card-card-cliques').hover(function () {
         demo_ca.animate({
             opacity: 0
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_cc.animate({
             opacity: 1
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_qc.animate({
             opacity: 0
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_link.attr('href', 'https://github.com/scottmangiapane/android-card-cliques');
     });
 
     $('#card-quackcryption').hover(function () {
         demo_ca.animate({
             opacity: 0
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_cc.animate({
             opacity: 0
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_qc.animate({
             opacity: 1
-        }, {duration: 250, queue: false});
+        }, {duration: 220, queue: false});
         demo_link.attr('href', 'https://github.com/scottmangiapane/android-quackcryption');
     });
 });
