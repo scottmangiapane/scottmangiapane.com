e23945e74a31549c80284324153cc94a4df331e8
Scott Mangiapane <scottmangiapane@gmail.com>
2016-09-15 19:09:12 -0400

    GitHub logo on demo; require-js.js
    Added an animated link to GitHub that appears when the demo is hovered
    over; added require-js.js which hides individual elements when JS is
    disabled.

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -3,46 +3,50 @@ $(document).ready(function () {
     var demo_ca = $('#demo-calculator-v2');
     var demo_cc = $('#demo-card-cliques');
     var demo_qc = $('#demo-quackcryption');
+    var demo_link = $('#demo-github');
 
     demo_cc.css({opacity: 0});
     demo_qc.css({opacity: 0});
     demo_js.animate({
         opacity: 0
-    }, {duration: 200, queue: false});
+    }, {duration: 250, queue: false});
 
     $('#card-calculator-v2').hover(function () {
         demo_ca.animate({
             opacity: 1
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
         demo_cc.animate({
             opacity: 0
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
         demo_qc.animate({
             opacity: 0
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
+        demo_link.attr('href', 'https://github.com/scottmangiapane/android-calculator-v2');
     });
 
     $('#card-card-cliques').hover(function () {
         demo_ca.animate({
             opacity: 0
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
         demo_cc.animate({
             opacity: 1
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
         demo_qc.animate({
             opacity: 0
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
+        demo_link.attr('href', 'https://github.com/scottmangiapane/android-card-cliques');
     });
 
     $('#card-quackcryption').hover(function () {
         demo_ca.animate({
             opacity: 0
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
         demo_cc.animate({
             opacity: 0
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
         demo_qc.animate({
             opacity: 1
-        }, {duration: 200, queue: false});
+        }, {duration: 250, queue: false});
+        demo_link.attr('href', 'https://github.com/scottmangiapane/android-quackcryption');
     });
 });
---
+++ assets/js/require-js.js
@@ -0,0 +1,1 @@
+$('.require-js').css('display', 'block')
