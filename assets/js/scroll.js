2e499f128ecbd00dc6846acfbc0ed1caa98b4543
Scott Mangiapane <scottmangiapane@gmail.com>
2016-11-14 15:27:12 -0500

    Changed colors and added parallax affect

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,3 +4,12 @@ $('a').click(function () {
     }, 500);
     return false;
 });
+
+$(function () {
+    $(window).scroll(function () {
+        var currentScrollTop = $(window).scrollTop();
+        var height = $(window).height();
+        var header = $('#header');
+        header.css('transform', 'scale(' + ((height - currentScrollTop) / height) + ')');
+    });
+});
