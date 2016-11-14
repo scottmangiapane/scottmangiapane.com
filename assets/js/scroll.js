44993941f176e52eb180dc4c9e39953816e3b60d
Scott Mangiapane <scottmangiapane@gmail.com>
2016-11-14 17:29:55 -0500

    Removed parallax for performance

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,12 +4,3 @@ $('a').click(function () {
     }, 500);
     return false;
 });
-
-$(function () {
-    $(window).scroll(function () {
-        var currentScrollTop = $(window).scrollTop();
-        var height = $(window).height();
-        var header = $('#header');
-        header.css('transform', 'scale(' + ((height - currentScrollTop) / height) + ')');
-    });
-});
