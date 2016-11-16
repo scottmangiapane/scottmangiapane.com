c6d89f5fed1edc8441cf7384aae384503315b371
Scott Mangiapane <scottmangiapane@gmail.com>
2016-11-15 23:29:59 -0500

    Revert "Made the footer stationary; fixed popup bug"
    This reverts commit 97163f916240ab5759fd9ebe42601c8609b5fc6c.

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -4,19 +4,3 @@ $('a').click(function () {
     }, 500);
     return false;
 });
-
-var footer = $('footer');
-var isVisible = false;
-console.log("Initialized");
-
-$(window).scroll(function () {
-    var scrollTop = $(window).scrollTop();
-    if (scrollTop > 200 && isVisible === false) {
-        isVisible = true;
-        footer.css('display', 'inherit');
-    } else if (scrollTop <= 200 && isVisible === true) {
-        isVisible = false;
-        footer.css('display', 'none');
-        console.log("Not visible");
-    }
-});
