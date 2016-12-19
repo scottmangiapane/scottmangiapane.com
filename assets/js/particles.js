953651c6c69ec86ae32af89d2fe89370db6bde6f
Scott Mangiapane <scottmangiapane@gmail.com>
2016-12-18 23:54:06 -0700

    Switched from squares to circles

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,4 +1,4 @@
-const particleSize = 100;
+const radius = 140;
 
 const display = document.getElementById("canvas");
 const displayWidth = display.width = window.innerWidth * 2;
@@ -15,7 +15,7 @@ document.onmousemove = function (event) {
 
 const particles = [];
 
-for (let i = 0; i < 100; i++)
+for (let i = 0; i < 60; i++)
     particles.push({
         xPosition: Math.random() * displayWidth,
         yPosition: Math.random() * displayHeight,
@@ -31,24 +31,24 @@ function frame() {
     particles.forEach(function (p) {
         p.xPosition += p.xVelocity;
         p.yPosition += p.yVelocity;
-        if (p.xPosition > displayWidth)
-            p.xPosition = -particleSize;
-        if (p.yPosition > displayHeight)
-            p.yPosition = -particleSize;
-        if (p.xPosition < -particleSize)
-            p.xPosition = displayWidth;
-        if (p.yPosition < -particleSize)
-            p.yPosition = displayHeight;
+        if (p.xPosition - radius > displayWidth)
+            p.xPosition = -radius;
+        if (p.yPosition - radius > displayHeight)
+            p.yPosition = -radius;
+        if (p.xPosition < -radius)
+            p.xPosition = displayWidth + radius;
+        if (p.yPosition < -radius)
+            p.yPosition = displayHeight + radius;
         context.beginPath();
         let distance = Math.sqrt(Math.pow(p.xPosition - mouse.x * 2, 2) + Math.pow(p.yPosition - mouse.y * 2, 2));
         if (distance < 400) {
             let c = parseInt(136 + 102 * distance / 400);
-            context.fillStyle = "rgba(" + c + " ," + c + ", " + c + ", 0.8)";
-            let offset = (400 - distance) / 4;
-            context.rect(p.xPosition - offset / 2, p.yPosition - offset / 2, particleSize + offset, particleSize + offset);
+            context.fillStyle = "rgba(" + c + " ," + c + ", " + c + ", 0.6)";
+            let offset = (400 - distance) / 8;
+            context.arc(p.xPosition + offset / 2, p.yPosition + offset / 2, radius - offset, 0, 2 * Math.PI);
         } else {
-            context.fillStyle = "rgba(238, 238, 238, 0.8)";
-            context.rect(p.xPosition, p.yPosition, particleSize, particleSize);
+            context.fillStyle = "rgba(238, 238, 238, 0.6)";
+            context.arc(p.xPosition, p.yPosition, radius, 0, 2 * Math.PI);
         }
         context.fill();
     });
