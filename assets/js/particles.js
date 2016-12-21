8402a7c8d3b366615e0e93eacdc08f41d4dae63d
Scott Mangiapane <scottmangiapane@gmail.com>
2016-12-20 23:03:45 -0700

    Disabled mouse events on mobile

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -1,4 +1,4 @@
-const radius = 100;
+const blockSize = 100;
 
 const display = document.getElementById("canvas");
 let displayWidth = display.width = window.innerWidth * 2;
@@ -7,18 +7,28 @@ let displayHeight = display.height = window.innerHeight * 2;
 const context = display.getContext("2d");
 
 const mouse = [];
+mouse.x = window.innerWidth / 2;
+mouse.y = window.innerHeight / 2;
 
 document.onmousemove = function (event) {
-    mouse.x = event.clientX;
-    mouse.y = event.clientY;
+    if (window.innerWidth >= 550) {
+        mouse.x = event.clientX;
+        mouse.y = event.clientY;
+    }
 };
 
 document.onmouseout = function () {
-    mouse.x = undefined;
-    mouse.y = undefined;
+    if (window.innerWidth >= 550) {
+        mouse.x = undefined;
+        mouse.y = undefined;
+    }
 };
 
 window.onresize = function () {
+    if (window.innerWidth < 550) {
+        mouse.x = window.innerWidth / 2;
+        mouse.y = window.innerHeight / 2;
+    }
     displayWidth = display.width = window.innerWidth * 2;
     displayHeight = display.height = window.innerHeight * 2;
 };
@@ -29,8 +39,8 @@ for (let i = 0; i < 200; i++)
     particles.push({
         xPosition: Math.random() * displayWidth,
         yPosition: Math.random() * displayHeight,
-        xVelocity: (Math.random() * displayWidth - displayWidth / 2) / 500,
-        yVelocity: (Math.random() * displayHeight - displayHeight / 2) / 500,
+        xVelocity: (Math.random() * 6 - 3),
+        yVelocity: (Math.random() * 6 - 3),
     });
 
 requestAnimationFrame(frame);
@@ -41,24 +51,24 @@ function frame() {
     particles.forEach(function (p) {
         p.xPosition += p.xVelocity;
         p.yPosition += p.yVelocity;
-        if (p.xPosition - radius > displayWidth)
-            p.xPosition = -radius;
-        if (p.yPosition - radius > displayHeight)
-            p.yPosition = -radius;
-        if (p.xPosition < -radius)
-            p.xPosition = displayWidth + radius;
-        if (p.yPosition < -radius)
-            p.yPosition = displayHeight + radius;
+        if (p.xPosition > displayWidth)
+            p.xPosition = -blockSize;
+        if (p.yPosition > displayHeight)
+            p.yPosition = -blockSize;
+        if (p.xPosition < -blockSize)
+            p.xPosition = displayWidth;
+        if (p.yPosition < -blockSize)
+            p.yPosition = displayHeight;
         context.beginPath();
         let distance = Math.sqrt(Math.pow(p.xPosition - mouse.x * 2, 2) + Math.pow(p.yPosition - mouse.y * 2, 2));
         if (distance < 400) {
             let c = parseInt(136 + 102 * distance / 400);
             context.fillStyle = "rgba(" + c + " ," + c + ", " + c + ", 0.6)";
             let offset = (400 - distance) / 8;
-            context.rect(p.xPosition + offset / 2, p.yPosition + offset / 2, radius - offset, radius - offset);
+            context.rect(p.xPosition + offset / 2, p.yPosition + offset / 2, blockSize - offset, blockSize - offset);
         } else {
             context.fillStyle = "rgba(238, 238, 238, 0.6)";
-            context.rect(p.xPosition, p.yPosition, radius, radius);
+            context.rect(p.xPosition, p.yPosition, blockSize, blockSize);
         }
         context.fill();
     });
