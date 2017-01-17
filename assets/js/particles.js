23b383b619436b2b2eef664bad3f097fa17ce4cc
Scott Mangiapane <scottmangiapane@gmail.com>
2017-01-17 11:35:47 -0500

    Number of particles and max speed of particles are now responsive

--- assets/js/particles.js
+++ assets/js/particles.js
@@ -5,35 +5,35 @@ const mouse = Object({});
 let displayHeight = display.height = window.innerHeight * 2;
 let displayWidth = display.width = window.innerWidth * 2;
 let blockSize = (displayWidth + displayHeight) / 40;
+let maxSpeed = blockSize / 20;
+let mouseRadius = 400;
+
+const numberOfParticles = (displayWidth * displayHeight) / (3 * blockSize * blockSize);
+const particles = [];
 
 mouse.x = undefined;
 mouse.y = undefined;
 
 document.onmousemove = (event) => {
-    if (window.innerWidth >= 550) {
-        mouse.x = event.clientX * 2;
-        mouse.y = event.clientY * 2;
-    }
+    mouse.x = event.clientX * 2;
+    mouse.y = event.clientY * 2;
 };
 
 document.onmouseout = () => {
-    if (window.innerWidth >= 550) {
-        mouse.x = undefined;
-        mouse.y = undefined;
-    }
+    mouse.x = undefined;
+    mouse.y = undefined;
 };
 
 window.onresize = () => {
     displayWidth = display.width = window.innerWidth * 2;
     displayHeight = display.height = window.innerHeight * 2;
     blockSize = (displayWidth + displayHeight) / 40;
+    maxSpeed = blockSize / 20;
 };
 
-const particles = [];
-
-for (let i = 0; i < 200; i++) {
-    let xVelocity = Math.random() * 6 - 3;
-    let yVelocity = Math.random() * 6 - 3;
+for (let i = 0; i < numberOfParticles; i++) {
+    let xVelocity = Math.random() * maxSpeed - maxSpeed / 2;
+    let yVelocity = Math.random() * maxSpeed - maxSpeed / 2;
     particles.push({
         xPosition: Math.random() * displayWidth,
         yPosition: Math.random() * displayHeight,
@@ -62,18 +62,18 @@ function frame() {
             p.yPosition = displayHeight;
         context.beginPath();
         let distance = Math.sqrt(Math.pow(p.xPosition - mouse.x, 2) + Math.pow(p.yPosition - mouse.y, 2));
-        if (distance < 400) {
-            p.xPosition - mouse.x > 0 ? p.xVelocity++ : p.xVelocity--;
-            p.yPosition - mouse.y > 0 ? p.yVelocity++ : p.yVelocity--;
-            let color = parseInt(200 * distance / 400);
-            let offset = (400 - distance) / 4;
+        if (distance < mouseRadius) {
+            p.xPosition - mouse.x > 0 ? p.xVelocity += 0.5 : p.xVelocity -= 0.5;
+            p.yPosition - mouse.y > 0 ? p.yVelocity += 0.5 : p.yVelocity -= 0.5;
+            let color = parseInt(200 * distance / mouseRadius);
+            let offset = (mouseRadius - distance) / 4;
             context.fillStyle = "rgba(" + color + " ," + color + ", " + color + ", 0.1)";
             context.rect(p.xPosition + offset / 2, p.yPosition + offset / 2, blockSize - offset, blockSize - offset);
         } else {
             if (p.xVelocity !== p.xVelocityBase)
-                (p.xVelocity > p.xVelocityBase) ? p.xVelocity-- : p.xVelocity++;
+                (p.xVelocity > p.xVelocityBase) ? p.xVelocity -= 0.5 : p.xVelocity += 0.5;
             if (p.yVelocity !== p.yVelocityBase)
-                (p.yVelocity > p.yVelocityBase) ? p.yVelocity-- : p.yVelocity++;
+                (p.yVelocity > p.yVelocityBase) ? p.yVelocity -= 0.5 : p.yVelocity += 0.5;
             context.fillStyle = "rgba(200, 200, 200, 0.1)";
             context.rect(p.xPosition, p.yPosition, blockSize, blockSize);
         }
