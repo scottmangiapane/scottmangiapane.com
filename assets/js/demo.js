b91b111e4455b8620633c78c716596376f3939a7
Scott Mangiapane <scottmangiapane@gmail.com>
2017-09-11 00:06:49 -0400

    Rearranged projects

--- assets/js/demo.js
+++ assets/js/demo.js
@@ -1,52 +1,61 @@
 $(document).ready(function () {
-    var demo_ca = $('#demo-calculator-v2');
-    var demo_cc = $('#demo-card-cliques');
-    var demo_qc = $('#demo-quackcryption');
-    var demo_nix = $('#demo-nix-js');
+    var demo_card_cliques = $('#demo-card-cliques');
+    var demo_quackcryption = $('#demo-quackcryption');
+    var demo_calculator_v2 = $('#demo-calculator-v2');
     var demo_truth_table_generator = $('#demo-truth-table-generator');
+    var demo_nix_js = $('#demo-nix-js');
     var demo_ti_games = $('#demo-ti-games');
     var demo_alphacs = $('#demo-alphacs');
     var demo_ti_terminal = $('#demo-ti-terminal');
 
-    demo_cc.css({ opacity: 0 });
-    demo_qc.css({ opacity: 0 });
-    demo_truth_table_generator.css({ opacity: 0 });
+    demo_quackcryption.css({ opacity: 0 });
+    demo_calculator_v2.css({ opacity: 0 });
+    demo_nix_js.css({ opacity: 0 });
     demo_alphacs.css({ opacity: 0 });
     demo_ti_terminal.css({ opacity: 0 });
 
-    $('#card-calculator-v2').hover(function () {
-        demo_ca.animate({
-            opacity: 1
-        }, { duration: 220, queue: false });
-        demo_cc.animate({
-            opacity: 0
-        }, { duration: 220, queue: false });
-        demo_qc.animate({
-            opacity: 0
-        }, { duration: 220, queue: false });
-    });
-
     $('#card-card-cliques').hover(function () {
-        demo_ca.animate({
-            opacity: 0
-        }, { duration: 220, queue: false });
-        demo_cc.animate({
+        demo_card_cliques.animate({
             opacity: 1
         }, { duration: 220, queue: false });
-        demo_qc.animate({
+        demo_quackcryption.animate({
+            opacity: 0
+        }, { duration: 220, queue: false });
+        demo_calculator_v2.animate({
             opacity: 0
         }, { duration: 220, queue: false });
     });
 
     $('#card-quackcryption').hover(function () {
-        demo_ca.animate({
+        demo_card_cliques.animate({
             opacity: 0
         }, { duration: 220, queue: false });
-        demo_cc.animate({
-            opacity: 0
-        }, { duration: 220, queue: false });
-        demo_qc.animate({
+        demo_quackcryption.animate({
             opacity: 1
+        }, { duration: 220, queue: false });
+        demo_calculator_v2.animate({
+            opacity: 0
+        }, { duration: 220, queue: false });
+    });
+
+    $('#card-calculator-v2').hover(function () {
+        demo_card_cliques.animate({
+            opacity: 0
+        }, { duration: 220, queue: false });
+        demo_quackcryption.animate({
+            opacity: 0
+        }, { duration: 220, queue: false });
+        demo_calculator_v2.animate({
+            opacity: 1
+        }, { duration: 220, queue: false });
+    });
+
+    $('#card-truth-table-generator').hover(function () {
+        demo_truth_table_generator.animate({
+            opacity: 1
+        }, { duration: 220, queue: false });
+        demo_nix_js.animate({
+            opacity: 0
         }, { duration: 220, queue: false });
     });
 
@@ -54,16 +63,7 @@ $(document).ready(function () {
         demo_truth_table_generator.animate({
             opacity: 0
         }, { duration: 220, queue: false });
-        demo_nix.animate({
-            opacity: 1
-        }, { duration: 220, queue: false });
-    });
-
-    $('#card-truth-table-generator').hover(function () {
-        demo_nix.animate({
-            opacity: 0
-        }, { duration: 220, queue: false });
-        demo_truth_table_generator.animate({
+        demo_nix_js.animate({
             opacity: 1
         }, { duration: 220, queue: false });
     });
