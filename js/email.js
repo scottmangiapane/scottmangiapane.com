8caab24736cb93331b721d92ff70c1c7d1c6c818
Scott Mangiapane <scottmangiapane@gmail.com>
2024-04-26 23:01:34 -0500

    New loading bars and less bootstrap dependency

--- js/email.js
+++ js/email.js
@@ -5,5 +5,5 @@ at = '&#x00040;';
 site = 'gmail.com';
 document.write("<span class='popup-wrapper'><a class='button' href='"
     + tag + colon + user + at + site
-    + "'>" + "E-MAIL" + "</a><span class='popup'><p class='remove-margins'>"
+    + "'>" + "Email" + "</a><span class='popup'><p class='remove-margins'>"
     + user + at + site + "</p></span></span>");
