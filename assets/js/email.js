99ad941818da620e3f87161b7a3b979309fae8fe
Scott Mangiapane <scottmangiapane@gmail.com>
2024-04-28 01:50:36 -0500

    Restored branch to its 2018 state (updates will continue on master)

---
+++ assets/js/email.js
@@ -0,0 +1,9 @@
+tag = 'mailto';
+colon = '&#x0003a;';
+user = 'scottmangiapane';
+at = '&#x00040;';
+site = 'gmail.com';
+document.write("<span class='popup-wrapper'><a class='button' href='"
+    + tag + colon + user + at + site
+    + "'>" + "E-MAIL" + "</a><span class='popup'><p class='remove-margins'>"
+    + user + at + site + "</p></span></span>");
